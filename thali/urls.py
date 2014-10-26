d8eadc259ba8cf6bae8f5846de258a89bb0a39c3
rdutta <rdutta@rdutta-VirtualBox.(none)>
2014-10-26 17:21:01 -0500

    UI is present

--- thali/urls.py
+++ thali/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic.base import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/',TemplateView.as_view(template_name="homefood.html")),
 )
