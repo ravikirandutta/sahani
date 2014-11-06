24645e910e0083e22f221d9952b3dd213d0c3b72
Keerthi <varrekeerthi@gmail.com>
2014-11-06 13:24:59 -0600

    Login and register

---
+++ bento/static/design/js/thali/thali.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(e){
+ 
+   $('#dropdown').on('click',function(){
+ 
+      $('.dropdownwrap').slideToggle();
+   });
+   }) 
+$(document).ready(function(e){
+ 
+   $('#signup').on('click',function(){
+ 
+      $('#signupbox').slideToggle();
+   });
+   }) 
+
+$(document).ready(function(e){
+ 
+   $('#presignup-message2').on('click',function(){
+ 
+      $('#notification').slideToggle();
+ 
+   });
+ 
+})
+$(document).ready(function(e){
+ 
+   $('#login').on('click',function(){
+ 
+      $('#loginbox').slideToggle();
+ 
+   });
+ 
+})
